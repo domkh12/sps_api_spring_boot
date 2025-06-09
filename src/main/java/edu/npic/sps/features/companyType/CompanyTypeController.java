6acd36c077159a30fce8461f336a469e45d5716c
domkh12 <dommr86@gmail.com>
2025-06-10 00:10:10 +0700

    update

--- src/main/java/edu/npic/sps/features/companyType/CompanyTypeController.java
+++ src/main/java/edu/npic/sps/features/companyType/CompanyTypeController.java
@@ -1,11 +1,48 @@
 package edu.npic.sps.features.companyType;
 
+import edu.npic.sps.domain.CompanyType;
+import edu.npic.sps.features.companyType.dto.CompanyTypeRequest;
+import edu.npic.sps.features.companyType.dto.CompanyTypeResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/company-types")
 @RequiredArgsConstructor
 public class CompanyTypeController {
+
+    private final CompanyTypeService companyTypeService;
+
+    @PreAuthorize("hasAuthority('ROLE_MANAGER')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{uuid}")
+    void delete(@PathVariable String uuid){
+        companyTypeService.delete(uuid);
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_MANAGER')")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PutMapping("/{uuid}")
+    CompanyTypeResponse update(@PathVariable String uuid, @Valid @RequestBody CompanyTypeRequest companyTypeRequest){
+        return companyTypeService.update(uuid, companyTypeRequest);
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_MANAGER')")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    CompanyTypeResponse create(@Valid @RequestBody CompanyTypeRequest companyTypeRequest){
+        return companyTypeService.create(companyTypeRequest);
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_MANAGER')")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping
+    List<CompanyTypeResponse> findAll(){
+        return  companyTypeService.findAll();
+    }
 }
