399d278d698bb18b9b84d0101c3375327f4a0681
domkh12 <dommr86@gmail.com>
2025-06-10 00:11:42 +0700

    update

--- src/main/java/edu/npic/sps/features/companyType/CompanyTypeController.java
+++ src/main/java/edu/npic/sps/features/companyType/CompanyTypeController.java
@@ -3,6 +3,7 @@ package edu.npic.sps.features.companyType;
 import edu.npic.sps.domain.CompanyType;
 import edu.npic.sps.features.companyType.dto.CompanyTypeRequest;
 import edu.npic.sps.features.companyType.dto.CompanyTypeResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/company-types")
 @RequiredArgsConstructor
+@Tag(name = "Company Type Management", description = "APIs for managing company type")
 public class CompanyTypeController {
 
     private final CompanyTypeService companyTypeService;
